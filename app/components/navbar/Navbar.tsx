80422abd5e9747a61a9108564536ac76ed0152da
MizanurRahmann <mizanur.rahman03032@gmail.com>
2023-04-29 19:41:18 +0600

    ADD: categories to the navigation.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import Categories from "./Categories";
 
 interface NavbarProps {
     currentUser?: SafeUser | null;
@@ -37,6 +38,7 @@ const Navbar: React.FC<NavbarProps> = ({currentUser}) => {
                     </div>
                 </Container>
             </div>
+            <Categories />
         </div>
     )
 }
